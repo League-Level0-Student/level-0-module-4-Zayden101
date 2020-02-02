9c62b5c4a6a821d8709d66ed4c424bfbe7a2d151
League Student <league@iMac-23.attlocal.net>
2020-02-02 12:27:51 -0800

    wwewrwrd
    dt

--- src/_07_sleepy_head/SleepyHead.java
+++ src/_07_sleepy_head/SleepyHead.java
@@ -18,13 +18,24 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
+        int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        int vacation = JOptionPane.showConfirmDialog(null, "is it vaction","Sleepy Head", JOptionPane.YES_NO_OPTION);
+        if (vacation==0 &&weekday==1) {
+        	JOptionPane.showMessageDialog(null, "DIE IN A OVER MICRAWAVED PIZZA");
 
+        }
+        if (vacation==1 &&weekday==0) {
+        	JOptionPane.showMessageDialog(null, "GET UP, THE POOP IS COMMING OUT!!!");
 
+        }
+        if (vacation==1 &&weekday==1) {
+        	JOptionPane.showMessageDialog(null, "u suk");
+
+        }
         /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
+         * Print “sleep in�? if it is a vacation or a weekend. 
+         * If it’s a weekday, print “get up lazybones!�? 
+         * If it is a weekday, and we are on vacation,print “sleep in�?.
          */
     }
 }
